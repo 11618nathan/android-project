e7e22d410de770c1571fcbebb9205a3f203ce8e9
11618nathan <11618nathan@gmail.com>
2018-05-09 00:44:26 +0900

    upload

---
+++ OffPlayer/app/src/main/java/com/example/user/offplayer/AudioService.java
@@ -0,0 +1,61 @@
+package com.example.user.offplayer;
+
+public class AudioService extends Service {
+    private final IBinder mBinder = new AudioServiceBinder();
+    private MediaPlayer mMediaPlayer;
+    private boolean isPrepared;
+
+    public class AudioServiceBinder extends Binder {
+        AudioService getService() {
+            return AudioService.this;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                isPrepared = true;
+                mp.start();
+            }
+        });
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                isPrepared = false;
+            }
+        });
+        mMediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                isPrepared = false;
+                return false;
+            }
+        });
+        mMediaPlayer.setOnSeekCompleteListener(new MediaPlayer.OnSeekCompleteListener() {
+            @Override
+            public void onSeekComplete(MediaPlayer mp) {
+
+            }
+        });
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mMediaPlayer != null) {
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+}
