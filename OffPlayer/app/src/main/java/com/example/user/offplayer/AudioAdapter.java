9b5a643c8f508a474fc76fa57adb0548e8c9791f
11618nathan <11618nathan@gmail.com>
2018-05-15 00:43:03 +0900

    update

--- OffPlayer/app/src/main/java/com/example/user/offplayer/AudioAdapter.java
+++ OffPlayer/app/src/main/java/com/example/user/offplayer/AudioAdapter.java
@@ -11,10 +11,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.squareup.picasso.Picasso;
-
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.zip.DataFormatException;
 
 public class AudioAdapter extends CursorRecyclerViewAdapter<RecyclerView.ViewHolder> {
@@ -57,6 +56,15 @@ public class AudioAdapter extends CursorRecyclerViewAdapter<RecyclerView.ViewHol
         }
     }
 
+    public ArrayList<Long> getAudioIds() {
+        int count = getItemCount();
+        ArrayList<Long> audioIds = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            audioIds.add(getItemId(i));
+        }
+        return audioIds;
+    }
+
     private class AudioViewHolder extends RecyclerView.ViewHolder {
         private final Uri artworkUri = Uri.parse("content://media/external/audio/albumart");
         private ImageView mImgAlbumArt;
@@ -75,7 +83,8 @@ public class AudioAdapter extends CursorRecyclerViewAdapter<RecyclerView.ViewHol
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    // ..
+                    AudioApplication.getInstance().getServiceInterface().setPlayList(getAudioIds()); // 재생목록등록
+                    AudioApplication.getInstance().getServiceInterface().play(mPosition); // 선택한 오디오재생
                 }
             });
         }
