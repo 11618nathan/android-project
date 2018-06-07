f99a78da3f0d587f104b3d58c3a92bec1f95a9bc
11618nathan <11618nathan@gmail.com>
2018-06-08 00:14:49 +0900

    Update

--- OffPlayer/app/src/main/java/com/example/user/offplayer/NotificationPlayer.java
+++ OffPlayer/app/src/main/java/com/example/user/offplayer/NotificationPlayer.java
@@ -6,12 +6,15 @@ import android.app.PendingIntent;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.widget.RemoteViews;
-
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 public class NotificationPlayer {
 
@@ -29,9 +32,62 @@ public class NotificationPlayer {
     }
 
     public void updateNotificationPlayer() {
+        /*
         cancel();
         mNotificationManagerBuilder = new NotificationManagerBuilder();
         mNotificationManagerBuilder.execute();
+        */
+        /*
+         */
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                Uri albumArtUri = ContentUris.withAppendedId(Uri.parse("content://media/external/audio/albumart"), mService.getAudioItem().mAlbumId);
+                Bitmap largIcon = null;
+                try {
+                    largIcon = Picasso.with(mService).load(albumArtUri).get();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                Intent actionTogglePlay = new Intent(CommandActions.TOGGLE_PLAY);
+                Intent actionForward = new Intent(CommandActions.FORWARD);
+                Intent actionRewind = new Intent(CommandActions.REWIND);
+                Intent actionClose = new Intent(CommandActions.CLOSE);
+                PendingIntent togglePlay = PendingIntent.getService(mService, 0, actionTogglePlay, 0);
+                PendingIntent forward = PendingIntent.getService(mService, 0, actionForward, 0);
+                PendingIntent rewind = PendingIntent.getService(mService, 0, actionRewind, 0);
+                PendingIntent close = PendingIntent.getService(mService, 0, actionClose, 0);
+
+
+                android.support.v4.app.NotificationCompat.Builder builder = new android.support.v4.app.NotificationCompat.Builder(mService);
+                builder
+                        .setContentTitle(mService.getAudioItem().mTitle)
+                        .setContentText(mService.getAudioItem().mArtist)
+                        .setLargeIcon(largIcon)
+                        .setContentIntent(PendingIntent.getActivity(mService, 0, new Intent(mService, MainActivity.class), 0));
+
+                builder.addAction(new android.support.v4.app.NotificationCompat.Action(R.drawable.ic_rewind, "", rewind));
+                builder.addAction(new android.support.v4.app.NotificationCompat.Action(mService.isPlaying() ? R.drawable.ic_pause : R.drawable.ic_play, "", togglePlay));
+                builder.addAction(new android.support.v4.app.NotificationCompat.Action(R.drawable.ic_forward, "", forward));
+                builder.addAction(new android.support.v4.app.NotificationCompat.Action(R.drawable.ic_close, "", close));
+                int[] actionsViewIndexs = new int[]{1,2,3};
+                builder.setStyle(new android.support.v4.media.app.NotificationCompat.MediaStyle().setShowActionsInCompactView(actionsViewIndexs));
+                builder.setSmallIcon(R.mipmap.ic_launcher);
+
+                Notification notification = builder.build();
+
+                NotificationManagerCompat.from(mService).notify(NOTIFICATION_PLAYER_ID, notification);
+
+                if (!isForeground) {
+                    isForeground = true;
+                    // 서비스를 Foreground 상태로 만든다
+                    mService.startForeground(NOTIFICATION_PLAYER_ID, notification);
+                }
+
+                return null;
+            }
+        }.execute();
     }
 
     public void removeNotificationPlayer() {
@@ -121,9 +177,9 @@ public class NotificationPlayer {
 
         private void updateRemoteView(RemoteViews remoteViews, Notification notification) {
             if (mService.isPlaying()) {
-                remoteViews.setImageViewResource(R.id.btn_play_pause, R.drawable.pause);
+                remoteViews.setImageViewResource(R.id.btn_play_pause, R.drawable.ic_pause);
             } else {
-                remoteViews.setImageViewResource(R.id.btn_play_pause, R.drawable.play);
+                remoteViews.setImageViewResource(R.id.btn_play_pause, R.drawable.ic_play);
             }
 
             String title = mService.getAudioItem().mTitle;
